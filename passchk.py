7eb5378192804c0796815e23182c6e2a3ff59d45
Greg Broiles <gbroiles@gmail.com>
2022-02-28 00:41:55 +0000

    add tabular output to passchk

--- passchk.py
+++ passchk.py
@@ -3,33 +3,55 @@
 import argparse
 import pwnedpasswords
 
+found = []
+notfound = []
+table = False
+
 
 def create_parse():
     parser = argparse.ArgumentParser(
         description="Have I Been Pwned bulk password checker"
     )
+    parser.add_argument("--table", help="output result as tables", action="store_true")
     parser.add_argument("filename", help="file(s) with passwords to scan", nargs="+")
     return parser
 
 
 def checkit(filename):
+    global table
     with open(filename, "r") as f:
         pws = f.read().splitlines()
     for pw in pws:
-        print(pw, end=" ")
+        if not table:
+            print(pw, end=" ")
         r = pwnedpasswords.check(pw)
         if r > 0:
-            print("breach(es) found")
+            found.append(pw)
+            if not table:
+                print("found")
         else:
-            print("not found")
+            notfound.append(pw)
+            if not table:
+                print("missing")
 
 
 def start():
+    global table
     parser = create_parse()
     args = parser.parse_args()
 
+    table = args.table
+
     for item in args.filename:
         checkit(item)
+
+    if table:
+        print("Found ", len(found), " ----------------------------------------")
+        for i in found:
+            print(i)
+        print("Missing ", len(notfound), " ----------------------------------------")
+        for i in notfound:
+            print(i)
 
 
 if __name__ == "__main__":
